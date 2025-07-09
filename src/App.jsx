c7ce5a5f901cc35daebf2e4d3200ba5ce2d1982e
franque199 <franque99@gmail.com>
2025-07-09 05:49:52 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Preflop Trainer Básico</h1>
+      <p>Projeto base corrigido para Vercel</p>
+    </div>
+  );
+}
+
+export default App;
